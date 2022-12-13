fd4b4f9f6fe34e8a629a1c77439633104641dd91
VictorManuelSalas <victorsalasnz@gmail.com>
2022-12-13 13:09:04 -0600

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import OneSignal from 'onesignal-cordova-plugin';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  signal_app_id = '41740cf0-fdcb-4d6b-8ea0-d96fb5431d9c';
+  firebase_id = '135841084689';
+
+  constructor(/*private oneSignal: OneSignal*/) {
+    /*this.oneSignal.startInit(this.signal_app_id, this.firebase_id);
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    this.oneSignal.handleNotificationReceived().subscribe((res) => {
+      // do something when notification is received
+      console.log(res);
+      
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe((res) => {
+      // do something when a notification is opened
+      console.log(res);
+    });
+
+    this.oneSignal.endInit(); */
+  }
+
+  ngOnInit(){
+      this.OneSignalInit();
+  }
+  // Call this function when your app starts
+  OneSignalInit(): void {
+    // Uncomment to set OneSignal device logging to VERBOSE  
+    // OneSignal.setLogLevel(6, 0);
+
+    // NOTE: Update the setAppId value below with your OneSignal AppId.
+    OneSignal.setAppId(this.signal_app_id);
+    OneSignal.setNotificationOpenedHandler(function (jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    });
+
+    // Prompts the user for notification permissions.
+    //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
+    OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
+      console.log("User accepted notifications: " + accepted);
+    });
+  }
 }
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
+  ngInit() {
+
+  }
+  Calcular() {
+    var num1 = <HTMLInputElement>document.getElementById('one');
+    var num2 = <HTMLInputElement>document.getElementById('two');
+    var resultado = <HTMLInputElement>document.getElementById('resultado');
+    var simbolo = <HTMLInputElement>document.getElementById('simbolo');
+
+    console.log(resultado);
+
+    let res = 0;
+
+    switch (simbolo.value) {
+      case '+':
+        res = parseInt(num1.value) + parseInt(num2.value);
+        resultado.innerHTML = "" + res;
+        break;
+      case '-':
+        res = parseInt(num1.value) - parseInt(num2.value);
+        resultado.innerHTML = "" + res;
+        break;
+      case '*':
+        res = parseInt(num1.value) * parseInt(num2.value);
+        resultado.innerHTML = "" + res;
+        break;
+      case '/':
+        res = parseInt(num1.value) / parseInt(num2.value);
+        resultado.innerHTML = "" + res;
+        break;
+      default:
+         resultado.innerHTML = "Syntax Error 😣";
+        break;
+    }
+
+  }
 }
